93df62a0510081a6582a6ad028dccef0dcc12c40
Shakib KATAYA <kataya.shakib@gmail.com>
2023-08-21 23:10:45 +0300

    Fix hydration failed error

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,6 @@
 import { exchangeAddress, exchangeAbi } from "@/constants";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
@@ -13,6 +14,9 @@ export default function Home() {
   // Check if the user's wallet is connected, and it's address using Wagmi's hooks.
   const { address, isConnected } = useAccount();
 
+  // State variable to know if the component has been mounted yet or not
+  const [isMounted, setIsMounted] = useState(false);
+
   // Fetch the balance of the user
   const balanceOfUser = useContractRead({
     abi: exchangeAbi,
@@ -20,6 +24,21 @@ export default function Home() {
     functionName: "balanceOf",
     args: [address],
   });
+
+  // Fetch the total supply
+  const totalSupply = useContractRead({
+    abi: exchangeAbi,
+    address: exchangeAddress,
+    functionName: "totalSupply",
+    args: [],
+  });
+
+  //is mounted useStaterequired
+  //Error: Hydration failed because the initial UI does not match what was rendered on the server.
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  if (!isMounted) return null;
 
   if (!isConnected)
     return (
@@ -34,6 +53,7 @@ export default function Home() {
             <ConnectButton />
           </div>
           <h1>Welcome to Crypto Devs DEX!</h1>
+          {process.env.projectId}
         </div>
       </div>
     );
@@ -46,7 +66,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.main}>
-        Your balance: {balanceOfUser.data.toString()}
+        hello Your balance: {balanceOfUser.data.toString()}
+        Total Supply: {totalSupply.data.toString()}
       </div>
     </div>
   );
